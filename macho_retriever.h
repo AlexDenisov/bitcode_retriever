8e6bfe106f561f8a63d637118fa35ac05b830ec4
Jonas Devlieghere <jonas@devlieghere.com>
2016-06-08 15:46:26 +0200

    Decouple bitcode retrieving and file creation

--- macho_retriever.h
+++ macho_retriever.h
@@ -4,12 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE *create_output_file(const char *cpuname);
-void extract_bitcode(FILE *stream, const char *cpuname, const uint32_t offset, const uint32_t size);
-void extract_bitcode_64(FILE *stream, const char *cpuname, const uint64_t offset, const uint64_t size);
-void retrieve_bitcode(FILE *stream, const int offset, const int swap_bytes);
-void retrieve_bitcode_64(FILE *stream, const int offset, const int swap_bytes);
-void retrieve_bitcode_from_nonfat(FILE *stream, const uint32_t offset);
-void retrieve_bitcode_from_fat(FILE *stream, const int swap_bytes);
+struct bitcode_t {
+  uint64_t size;
+  char* buffer;
+  char* arch;
+};
+
+struct bitcode_t* make_bitcode(FILE* stream, const char* cpuname, const uint64_t offset, const uint64_t size);
+struct bitcode_t* extract_bitcode(FILE* stream, const int offset, const int swap_bytes);
+struct bitcode_t* extract_bitcode_64(FILE* stream, const int offset, const int swap_bytes);
+
+struct bitcode_t* retrieve_bitcode_from_nonfat(FILE* stream, const uint32_t offset);
+
+void retrieve_bitcode(FILE* stream, struct bitcode_t* bitcodes[], int* count);
 
 #endif
--- main.c
+++ main.c
@@ -1,20 +1,46 @@
-#include "macho_retriever.h"
 #include "macho_reader.h"
+#include "macho_retriever.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+FILE *create_xar_file(const char *cpuname) {
+  const char *ext = ".xar";
+  int length = strlen(cpuname) + strlen(ext);
+  char *filename = calloc(sizeof(char), length);
+  strcpy(filename, cpuname);
+  strcat(filename, ext);
+
+  FILE *output = fopen(filename, "wb");
+  free(filename);
+
+  if (!output) {
+    printf("Cannot open '%s' for writing\n", filename);
+    exit(1);
+  }
+
+  return output;
+}
+
+void write_to_xar(struct bitcode_t *bitcode) {
+  FILE *output = create_xar_file(bitcode->arch);
+  fwrite(bitcode->buffer, sizeof(char), bitcode->size, output);
+  fclose(output);
+}
 
 int main(int argc, char *argv[]) {
   const char *filename = argv[1];
   FILE *stream = fopen(filename, "rb");
 
-  uint32_t magic = get_magic(stream, 0);
-  int fat = is_fat(magic);
-  int swap_bytes = is_should_swap_bytes(magic);
+  struct bitcode_t *bitcodes[4];
+  int count;
+  retrieve_bitcode(stream, bitcodes, &count);
 
-  if (fat) {
-    retrieve_bitcode_from_fat(stream, swap_bytes);
-  } else {
-    retrieve_bitcode_from_nonfat(stream, 0);
+  for (int i = 0; i < count; i++) {
+    if (bitcodes[i]) {
+      write_to_xar(bitcodes[i]);
+    }
   }
 
   fclose(stream);
